96f0ef424a069e433101f65f3ce5882c0293de86
Asawari <asawari.chingale@gmail.com>
2018-01-24 12:22:23 -0800

    Initial commit

---
+++ HomeInventory/Products.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeInventory
+{
+    /// <summary>
+    /// This represents the rooms in the house
+    /// </summary>
+    enum RoomType
+    {
+        Kitchen,
+        LivingRoom,
+        DiningRoom,
+        PowderRoom,
+        Office,
+        Foyer,
+        MasterBedroom,
+        MasterBath,
+        KidBedroom1,
+        KidBedroom2,
+        KidBath,
+        MediaRoom,
+        Gym,
+        GuestRoom,
+        PlayRoom,
+        BackYard,
+        Garage        
+    }
+    /// <summary>
+    /// This represents whether product was sold, donated or recycled
+    /// </summary>
+    enum RemoveType
+    {
+        Sale,
+        Donate,
+        Recycle
+    }
+    /// <summary>
+    /// This represents the products in the home
+    /// </summary>
+    class Products
+    {
+        #region Properties
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public DateTime DatePurchased { get; set; }
+
+        public Decimal PurchasedPrice { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public RoomType TypeOfRoom { get; set; }
+        public RemoveType TypeOfRemoval { get; set; }
+
+        public Decimal SalePrice { get; set; }
+
+        public Boolean Defected { get; set; }
+        #endregion
+
+        #region Methods
+
+        public Boolean IsExpired()
+        {
+            if (DateTime.Compare(DateTime.Now.Date, ExpiryDate.Date) == 0)
+                return true;
+            return false;
+        }
+
+        public Boolean IsDefected()
+        {
+            if (Defected)
+                return true;
+            return false;
+        }
+        #endregion
+
+
+    }
+}
---
+++ HomeInventory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Products();
+            product.ProductName = "Sofa";
+            product.PurchasedPrice = 1000M;
+            product.DatePurchased = new DateTime(2012, 1, 14);
+            product.ExpiryDate = new DateTime(2018, 1, 14);
+
+           // if (product.IsExpired())
+                Console.WriteLine($"PN:{product.ProductName}, bought at PP:{product.PurchasedPrice}, has expired today ED:{product.ExpiryDate.Date}");
+           
+        }
+    }
+}
